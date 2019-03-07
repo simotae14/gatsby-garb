1ffc81d4d9a541e798cf3baa961a6ce2a32fc3dd
Simone Taeggi <startae14@gmail.com>
2019-03-07 21:39:32 +0100

    Execute a Static Query to retrieve the Files

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Garb`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Simone Taeggi`,
+    createdAt: 2019,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
